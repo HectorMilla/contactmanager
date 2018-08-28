1f424c825de35e98f6fbd5413d6724683cfa87b0
hector milla <hectormilla8@gmail.com>
2018-08-28 13:16:19 -0400

    initial commit

---
+++ src/components/Contacts/AddContact_ref.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+
+class AddContact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.nameInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.phoneInput = React.createRef();
+  }
+
+  onSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  static defaultProps = {
+    name: "fred sss",
+    email: "email@leftnut.com",
+    phone: "333-333-3333"
+  };
+
+  render() {
+    const { name, email, phone } = this.props;
+    return (
+      <div className="card mb-3">
+        <div className="card-header">Add Contact</div>
+        <div className="card-body">
+          <form onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control form-control-lg"
+                placeholder="Enter Name..."
+                DefaultValue={name}
+                ref={this.nameInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control form-control-lg"
+                placeholder="Enter Email..."
+                DefaultValue={email}
+                ref={this.emailInput}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                className="form-control form-control-lg"
+                placeholder="Enter Phone..."
+                DefaultValue={phone}
+                ref={this.phoneInput}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
